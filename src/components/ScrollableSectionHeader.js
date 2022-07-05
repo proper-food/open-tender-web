2bf1d860205ac93b95a26e27f31c5e22f189c46c
JC Harrington <jc@brandibble.co>
2022-07-05 14:13:51 -0500

    Adjust styles of SeeMoreLink

--- src/components/ScrollableSectionHeader.js
+++ src/components/ScrollableSectionHeader.js
@@ -1,9 +1,8 @@
 import styled from '@emotion/styled'
 import propTypes from 'prop-types'
-import { Link } from 'react-router-dom'
 import { Heading } from '@open-tender/components'
 
-import iconMap from './iconMap'
+import SeeMoreLink from './SeeMoreLink'
 
 const ScrollableSectionHeaderView = styled.div`
   display: flex;
@@ -30,52 +29,12 @@ const ScrollableSectionTitle = styled.div`
   }
 `
 
-const ScrollableSectionLink = styled.div`
-  margin: 0 -0.5rem 0 0;
-
-  p {
-    line-height: 1;
-    font-size: ${(props) => props.theme.fonts.sizes.big};
-    @media (max-width: ${(props) => props.theme.breakpoints.mobile}) {
-      font-size: ${(props) => props.theme.fonts.sizes.small};
-    }
-
-    a {
-      display: flex;
-      justify-content: flex-end;
-      align-items: center;
-    }
-
-    span {
-      display: block;
-    }
-  }
-`
-
-const ScrollableSectionChevron = styled.span`
-  width: 1.4rem;
-  height: 1.4rem;
-  margin: 0.1rem 0 0 0;
-  color: ${(props) => props.theme.colors.primary};
-`
-
-const ScrollableSectionHeader = ({ title, to, style }) => (
+const ScrollableSectionHeader = ({ title, to, toText = 'View All', style }) => (
   <ScrollableSectionHeaderView style={style}>
     <ScrollableSectionTitle>
       <Heading as="p">{title}</Heading>
     </ScrollableSectionTitle>
-    {to && (
-      <ScrollableSectionLink>
-        <p>
-          <Link to={to}>
-            <Heading>View All</Heading>
-            <ScrollableSectionChevron>
-              {iconMap.ChevronRight}
-            </ScrollableSectionChevron>
-          </Link>
-        </p>
-      </ScrollableSectionLink>
-    )}
+    {to && <SeeMoreLink text={toText} to={to} />}
   </ScrollableSectionHeaderView>
 )
 
