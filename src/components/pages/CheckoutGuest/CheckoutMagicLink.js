8eadd35a05db2e35ee4037aae5f6ff6881d61042
JC Harrington <jc@brandibble.co>
2022-08-16 21:40:11 -0400

    Adjust CheckoutFlow messages

--- src/components/pages/CheckoutGuest/CheckoutMagicLink.js
+++ src/components/pages/CheckoutGuest/CheckoutMagicLink.js
@@ -3,28 +3,36 @@ import { fetchGuestThanx, resetGuest, selectGuest } from '@open-tender/redux'
 import { ButtonLink } from '@open-tender/components'
 
 import { PageTitle, PageContent } from '../..'
+import { selectContent } from '../../../slices'
+
+const defaultText = {
+  title: 'Account found!',
+  subtitle:
+    'We just sent you a magic sign in link! Please open your email on this device and click the link to sign in and continue your order.',
+  resend: 'Click here to resend the magic link.',
+  changeEmail: 'Or click here to enter a different email address.',
+}
 
 const CheckoutMagicLink = () => {
   const dispatch = useDispatch()
+  const { checkoutFlow } = useSelector(selectContent)
+  const config = checkoutFlow?.sections?.magicLink || defaultText
   const { email, loading } = useSelector(selectGuest)
   const resend = () => dispatch(fetchGuestThanx(email))
   const reset = () => dispatch(resetGuest())
 
   return (
     <>
-      <PageTitle
-        title="Account found!"
-        subtitle="We just sent you a magic sign in link! Please open your email on this device and click the link to sign in and continue your order."
-      />
+      <PageTitle {...config} />
       <PageContent>
         <p>
           <ButtonLink onClick={resend} disabled={loading === 'pending'}>
-            Click here to resend the magic link.
+            {config.resend}
           </ButtonLink>
         </p>
         <p>
           <ButtonLink onClick={reset} disabled={loading === 'pending'}>
-            Or click here to enter a different email address.
+            {config.changeEmail}
           </ButtonLink>
         </p>
       </PageContent>
