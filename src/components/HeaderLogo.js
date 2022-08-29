7f08483b4e8da1c53c946c833767e7d2033fa4ee
JC Harrington <jc@brandibble.co>
2022-08-29 14:57:30 -0400

    Remove useNavigate() from HeaderLogo

--- src/components/HeaderLogo.js
+++ src/components/HeaderLogo.js
@@ -1,10 +1,7 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { isMobile } from 'react-device-detect'
 import styled from '@emotion/styled'
-
+import { isMobile } from 'react-device-detect'
+import { useSelector } from 'react-redux'
 import { selectBrand } from '../slices'
-import { useNavigate } from 'react-router-dom'
 
 const HeaderLogoLink = styled.a`
   display: block;
@@ -19,32 +16,18 @@ const HeaderLogoLink = styled.a`
   }
 `
 
-const HeaderLogoButton = styled.button`
-  display: block;
-  max-width: 14rem;
-  margin: 0.4rem 0 0;
-  @media (max-width: ${(props) => props.theme.breakpoints.tablet}) {
-    max-width: 13rem;
-  }
-
-  img {
-    pointer-events: none;
-  }
-`
-
 const HeaderLogo = ({ useLight = false }) => {
-  const navigate = useNavigate()
   const { url, logo, logoLight } = useSelector(selectBrand)
   const logoUrl = useLight ? logoLight : logo
 
-  return url ? (
-    <HeaderLogoLink isMobile={isMobile} href={url} rel="noopener noreferrer">
+  return (
+    <HeaderLogoLink
+      isMobile={isMobile}
+      href={url || '/'}
+      rel="noopener noreferrer"
+    >
       <img src={logoUrl} alt="logo" />
     </HeaderLogoLink>
-  ) : (
-    <HeaderLogoButton onClick={() => navigate('/')}>
-      <img src={logoUrl} alt="logo" />
-    </HeaderLogoButton>
   )
 }
 
