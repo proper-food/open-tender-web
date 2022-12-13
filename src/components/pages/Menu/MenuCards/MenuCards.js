f0749d8e68e6aa6b877f890a089d8eff700a42c8
JC Harrington <jc@brandibble.co>
2022-12-12 21:06:28 -0500

    Update MenuCards

--- src/components/pages/Menu/MenuCards/MenuCards.js
+++ src/components/pages/Menu/MenuCards/MenuCards.js
@@ -2,31 +2,6 @@ import propTypes from 'prop-types'
 import styled from '@emotion/styled'
 import MenuCard from './MenuCard'
 
-// const MenuCardsView = styled.div`
-//   display: grid;
-//   justify-content: center;
-//   padding: 0;
-//   margin: 2rem 0 0;
-//   gap: ${(props) => props.theme.layout.padding};
-//   grid-template-columns: repeat(4, 1fr);
-//   @media (max-width: 1350px) {
-//     grid-template-columns: repeat(3, 1fr);
-//   }
-//   // @media (max-width: ${(props) => props.theme.breakpoints.narrow}) {
-//   //   grid-template-columns: repeat(2, 1fr);
-//   // }
-//   @media (max-width: ${(props) => props.theme.breakpoints.tablet}) {
-//     grid-template-columns: repeat(2, 1fr);
-//     gap: ${(props) => props.theme.layout.paddingMobile};
-//   }
-//   @media (max-width: 650px) {
-//     margin: 1rem 0 0;
-//     column-gap: 1.5rem;
-//     row-gap: ${(props) => props.theme.layout.paddingMobile};
-//     grid-template-columns: repeat(2, 1fr);
-//   }
-// `
-
 const MenuCardsView = styled.div`
   margin: 0 auto;
 `
